3bbf3284ac6346839d499808a4506310c127ba42
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-17 19:34:43 +0530

    Improve padding and add shadow

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,9 @@ const Main = styled.div`
   align-items: center;
   justify-content: center;
   background: white;
-  padding: 80px;
+  padding: 5%;
+  height: 100vh;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
 `;
 
 const Header = styled.h1`
